e90d3ab8a65ba2aff692a1d6fa2fb20ed0491c14
your name <you@example.com>
2021-03-29 18:45:06 +0200

    Initial commit for master\n Includes update for control patient SC2 results

---
+++ SharedCode/Normalize_EMG.m
@@ -0,0 +1,30 @@
+function [EMG] = Normalize_EMG(filename, data, MVC, plot_bool)
+
+    muscle_names = ["GM", "Sol", "TA"];
+    
+    EMG = data./MVC;
+    
+    save(strcat('./Output Data/Normalized EMG/', filename,'_Normalized'), 'EMG');
+    
+    if plot_bool == 1
+        figure()
+        subplot(3,1,1)
+        plot(EMG(:,1))
+        title(muscle_names(1))
+        grid on;
+        
+        subplot(3,1,2)
+        plot(EMG(:,2))
+        title(muscle_names(2))
+        grid on;
+        
+        subplot(3,1,3)
+        plot(EMG(:,3))
+        title(muscle_names(3))
+        grid on;
+        
+        sgtitle(strcat('Normalized EMG: ', filename))
+        
+        saveas(gcf, fullfile('./Output Data/Normalized EMG/', strcat(filename,'_Normalized')), 'fig');
+    end
+end
---
+++ Stroke Patients/SP2/Input Data/Markers/markers.m
@@ -0,0 +1,17 @@
+
+subject = '1';
+muscle_names = ["GM", "Sol", "TA1", "TA2"];
+degrees = ["0", "10", "20", "15"];
+
+for i=1:length(muscle_names)
+    for j=1:length(degrees)
+        filepath = strcat('SC', subject, '_MVC35_', degrees(j), 'deg_', muscle_names(i), '/');
+        load(strcat(filepath, 'Markers'))
+        figure()
+        subplot(3,1,1), plot(Markers.RawData(:,22))
+        title(strcat('SC', subject, ', ', degrees(j), 'deg, ', muscle_names(i)))
+        subplot(3,1,2), plot(Markers.RawData(:,23))
+        subplot(3,1,3), plot(Markers.RawData(:,24))
+        
+    end
+end
